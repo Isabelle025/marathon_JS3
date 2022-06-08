3f66ad2fbd81d148beca14ebd7a9d3deec8d7d1f
Isabelle025 <isagrangeret@gmail.com>
2022-06-08 11:52:00 +0200

    apparition sélection player

--- projet_html_code/script.js
+++ projet_html_code/script.js
@@ -16,3 +16,23 @@ closeRules.addEventListener('click', function()
     asideRules.style.opacity = 0;
     asideRules.style.zIndex = -1; 
 }) 
+
+const hands = document.querySelectorAll(".option-container");
+const step1 = document.querySelector(".step-1");
+const step2 = document.querySelector(".step-2");
+const playerSelection = document.querySelector(".player-pick-text");
+const houseSelection = document.querySelector(".house-pick-text");
+const results = document.querySelector(".annouce-winner");
+
+hands.forEach((hand) =>{
+    hand.addEventListener('click', function(){
+        
+        let handSelected = hand;
+        step1.style.opacity = 0;
+        step2.style.opacity = 1;
+        step2.style.transform = "scale(1, 1)";
+        results.style.display = "none";
+        //playerSelection.insertAdjacentElement("afterend",handSelected);//insère handSelected après la fin de playerSelection 
+        playerSelection.nextElementSibling.replaceWith(handSelected);
+    })
+})
